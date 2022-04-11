86c3fd5be0e336b7fc9e99afb68381ac75d62b1e
Johannes Elonheimo <johannes.elonheimo@gmail.com>
2022-04-11 12:01:43 +0300

    add invoke format

--- tasks.py
+++ tasks.py
@@ -15,3 +15,7 @@ def coverage_report(ctx):
 @task
 def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
+
+@task
+def format(ctx):
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
