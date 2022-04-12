af7f475ac22e0f39dd29735e0760a98c3c0e9d38
Johannes Elonheimo <johannes.elonheimo@gmail.com>
2022-04-12 16:23:42 +0300

    add config

---
+++ src/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+DIRNAME = os.path.dirname(__file__)
+
+try:
+    load_dotenv(dotenv_path=os.path.join(DIRNAME, "..", ".env"))
+except FileNotFoundError:
+    pass
+
+DB_NAME = os.getenv('DATABASE_FILENAME') or 'database.sqlite'
+DB_PATH = os.path.join(DIRNAME, DB_NAME)
+#DB_PATH = os.path.join(DIRNAME, '..', 'data', DB_NAME)
